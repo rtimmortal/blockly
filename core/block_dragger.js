ce90aac6a247c4c5adba5243b1f0722a6dee80a6
tengteng <tengteng@codemao.cn>
2018-08-09 14:26:00 +0800

    docs(block_dragger):translation

--- core/block_dragger.js
+++ core/block_dragger.js
@@ -34,29 +34,29 @@ goog.require('goog.asserts');
 
 
 /**
- * Class for a block dragger.  It moves blocks around the workspace when they
- * are being dragged by a mouse or touch.
- * @param {!Blockly.BlockSvg} block The block to drag.
- * @param {!Blockly.WorkspaceSvg} workspace The workspace to drag on.
+ * 关于积木拖动的类. 当积木被鼠标或者触控拖动时，积木会在工作空间上移动
+ * 
+ * @param {!Blockly.BlockSvg} block 被拖动的积木
+ * @param {!Blockly.WorkspaceSvg} workspace 积木被拖动的工作空间
  * @constructor
  */
 Blockly.BlockDragger = function(block, workspace) {
   /**
-   * The top block in the stack that is being dragged.
+   * 被拖动的积木栈的最顶层的积木
    * @type {!Blockly.BlockSvg}
    * @private
    */
   this.draggingBlock_ = block;
 
   /**
-   * The workspace on which the block is being dragged.
+   * 积木被拖动的工作空间
    * @type {!Blockly.WorkspaceSvg}
    * @private
    */
   this.workspace_ = workspace;
 
   /**
-   * Object that keeps track of connections on dragged blocks.
+   * 一个保持对被拖动的积木组合之间的连接的追踪的对象
    * @type {!Blockly.DraggedConnectionManager}
    * @private
    */
@@ -64,7 +64,7 @@ Blockly.BlockDragger = function(block, workspace) {
       this.draggingBlock_);
 
   /**
-   * Which delete area the mouse pointer is over, if any.
+   * 鼠标指针所在的删除区域（如果有）
    * One of {@link Blockly.DELETE_AREA_TRASH},
    * {@link Blockly.DELETE_AREA_TOOLBOX}, or {@link Blockly.DELETE_AREA_NONE}.
    * @type {?number}
@@ -73,24 +73,24 @@ Blockly.BlockDragger = function(block, workspace) {
   this.deleteArea_ = null;
 
   /**
-   * Whether the block would be deleted if dropped immediately.
+   * 积木如果被立刻拖动是否会被删除
    * @type {boolean}
    * @private
    */
   this.wouldDeleteBlock_ = false;
 
   /**
-   * The location of the top left corner of the dragging block at the beginning
-   * of the drag in workspace coordinates.
+   *被拖动积木在被开始拖动时，积木左上角在工作空间上的坐标定位
+   *
    * @type {!goog.math.Coordinate}
    * @private
    */
   this.startXY_ = this.draggingBlock_.getRelativeToSurfaceXY();
 
   /**
-   * A list of all of the icons (comment, warning, and mutator) that are
-   * on this block and its descendants.  Moving an icon moves the bubble that
-   * extends from it if that bubble is open.
+   * 这个积木以及其衍生出的积木上的所有图标(comment, warning, and mutator)的列表
+   * 如果bubble是打开的，移动一个图标时就会移动这个图标衍生出来的bubble
+   * 
    * @type {Array.<!Object>}
    * @private
    */
@@ -98,7 +98,7 @@ Blockly.BlockDragger = function(block, workspace) {
 };
 
 /**
- * Sever all links from this object.
+ * 切断这个对象的所有连接
  * @package
  */
 Blockly.BlockDragger.prototype.dispose = function() {
@@ -114,15 +114,15 @@ Blockly.BlockDragger.prototype.dispose = function() {
 };
 
 /**
- * Make a list of all of the icons (comment, warning, and mutator) that are
- * on this block and its descendants.  Moving an icon moves the bubble that
- * extends from it if that bubble is open.
- * @param {!Blockly.BlockSvg} block The root block that is being dragged.
- * @return {!Array.<!Object>} The list of all icons and their locations.
+   * 这个积木以及其衍生出的积木上的所有图标(comment, warning, and mutator)的列表
+   * 如果bubble是打开的，移动一个图标时就会移动这个图标衍生出来的bubble
+ * 
+ * @param {!Blockly.BlockSvg} block 被拖动的根积木
+ * @return {!Array.<!Object>} 包含所有图标及其定位的列表
  * @private
  */
 Blockly.BlockDragger.initIconData_ = function(block) {
-  // Build a list of icons that need to be moved and where they started.
+  // 建立一个需要被移动的图标和其开始位置的列表
   var dragIconData = [];
   var descendants = block.getDescendants();
   for (var i = 0, descendant; descendant = descendants[i]; i++) {
@@ -141,10 +141,9 @@ Blockly.BlockDragger.initIconData_ = function(block) {
 };
 
 /**
- * Start dragging a block.  This includes moving it to the drag surface.
- * @param {!goog.math.Coordinate} currentDragDeltaXY How far the pointer has
- *     moved from the position at mouse down, in pixel units.
- * @param {boolean} healStack whether or not to heal the stack after disconnecting
+ * 开始拖动一个积木。  这包括将其移动到拖动层
+ * @param {!goog.math.Coordinate} currentDragDeltaXY 指针从开始到停止的移动距离(像素单位)
+ * @param {boolean} healStack 断开连接后是否要恢复堆栈
  * @package
  */
 Blockly.BlockDragger.prototype.startBlockDrag = function(currentDragDeltaXY, healStack) {
@@ -180,11 +179,10 @@ Blockly.BlockDragger.prototype.startBlockDrag = function(currentDragDeltaXY, hea
 };
 
 /**
- * Execute a step of block dragging, based on the given event.  Update the
- * display accordingly.
- * @param {!Event} e The most recent move event.
- * @param {!goog.math.Coordinate} currentDragDeltaXY How far the pointer has
- *     moved from the position at the start of the drag, in pixel units.
+ * 基于给出的事件执行积木拖动.相应地更新显示
+ * 
+ * @param {!Event} e 最近的拖动事件
+ * @param {!goog.math.Coordinate} currentDragDeltaXY 指针从开始到停止的移动距离(像素单位)
  * @package
  */
 Blockly.BlockDragger.prototype.dragBlock = function(e, currentDragDeltaXY) {
@@ -201,10 +199,9 @@ Blockly.BlockDragger.prototype.dragBlock = function(e, currentDragDeltaXY) {
 };
 
 /**
- * Finish a block drag and put the block back on the workspace.
- * @param {!Event} e The mouseup/touchend event.
- * @param {!goog.math.Coordinate} currentDragDeltaXY How far the pointer has
- *     moved from the position at the start of the drag, in pixel units.
+ * 结束积木的拖动并且把积木放回工作空间
+ * @param {!Event} e 鼠标松开/触摸停止事件
+ * @param {!goog.math.Coordinate} currentDragDeltaXY 指针从开始到停止的移动距离(像素单位)
  * @package
  */
 Blockly.BlockDragger.prototype.endBlockDrag = function(e, currentDragDeltaXY) {
@@ -240,7 +237,7 @@ Blockly.BlockDragger.prototype.endBlockDrag = function(e, currentDragDeltaXY) {
 };
 
 /**
- * Fire a move event at the end of a block drag.
+ * 在积木拖动结束时触发移动事件。
  * @private
  */
 Blockly.BlockDragger.prototype.fireMoveEvent_ = function() {
@@ -251,9 +248,9 @@ Blockly.BlockDragger.prototype.fireMoveEvent_ = function() {
 };
 
 /**
- * Shut the trash can and, if necessary, delete the dragging block.
- * Should be called at the end of a block drag.
- * @return {boolean} whether the block was deleted.
+ * 关闭垃圾桶并且在需要的时候删除正在拖动的积木
+ * 应该在积木拖动结束时调用
+ * @return {boolean} 积木是否被删除
  * @private
  */
 Blockly.BlockDragger.prototype.maybeDeleteBlock_ = function() {
@@ -263,19 +260,18 @@ Blockly.BlockDragger.prototype.maybeDeleteBlock_ = function() {
     if (trashcan) {
       goog.Timer.callOnce(trashcan.close, 100, trashcan);
     }
-    // Fire a move event, so we know where to go back to for an undo.
+    // 触发一个移动事件, 所以我们知道应该撤回到哪里.
     this.fireMoveEvent_();
     this.draggingBlock_.dispose(false, true);
   } else if (trashcan) {
-    // Make sure the trash can is closed.
+    // 确保垃圾桶被关闭
     trashcan.close();
   }
   return this.wouldDeleteBlock_;
 };
 
 /**
- * Update the cursor (and possibly the trash can lid) to reflect whether the
- * dragging block would be deleted if released immediately.
+ * 更新光标(可能是垃圾桶的盖子)来反映正在被拖动的积木如果被立刻松开是否会被删除
  * @private
  */
 Blockly.BlockDragger.prototype.updateCursorDuringBlockDrag_ = function() {
@@ -295,10 +291,8 @@ Blockly.BlockDragger.prototype.updateCursorDuringBlockDrag_ = function() {
 };
 
 /**
- * Convert a coordinate object from pixels to workspace units, including a
- * correction for mutator workspaces.
- * This function does not consider differing origins.  It simply scales the
- * input's x and y values.
+ * 把一个坐标从像素单位转换成工作空间中的单位, 包括对于mutator workspaces的修正
+ * 这个函数不考虑differing origins.它只是简单的缩放x与y值
  * @param {!goog.math.Coordinate} pixelCoord A coordinate with x and y values
  *     in css pixel units.
  * @return {!goog.math.Coordinate} The input coordinate divided by the workspace
@@ -320,9 +314,8 @@ Blockly.BlockDragger.prototype.pixelsToWorkspaceUnits_ = function(pixelCoord) {
 };
 
 /**
- * Move all of the icons connected to this drag.
- * @param {!goog.math.Coordinate} dxy How far to move the icons from their
- *     original positions, in workspace units.
+ * 将所有图标都连接到拖动上
+ * @param {!goog.math.Coordinate} dxy 图标移动的距离(工作空间单位)
  * @private
  */
 Blockly.BlockDragger.prototype.dragIcons_ = function(dxy) {
